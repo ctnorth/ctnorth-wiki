c590d1b19b98e6f14868abd05b3dbb13d629ab1c
iFarbod <ifarbod@outlook.com>
2017-02-03 18:07:21 +0330

    Revert e6bf1db

--- syncwiki.bat
+++
@@ -1,8 +0,0 @@
-:: git remote add repo https://<username>:<token>@github.com/ctnorth/ctnorth-wiki.git
-:: git remote add wiki https://<username>:<token>@github.com/ctnorth/ctnorth.wiki.git
-
-@echo off
-git checkout master
-git fetch --all
-git merge --ff wiki/master
-git push origin master
